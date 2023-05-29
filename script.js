8be2788cbecf494988090676e69690060e6f2f7b
Vlad K <110909601+ch1kim0n1@users.noreply.github.com>
2023-05-29 17:41:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,33 +11,42 @@ window.addEventListener('DOMContentLoaded', function() {
         var currentEvent = data.items[0];
         var futureEvent = data.items[1];
   
-        //current event information
+        // Current event information
         var currentEventName = currentEvent.summary;
         var currentEventEndDate = new Date(currentEvent.end.dateTime);
         var currentEventImage = currentEvent.attachments && currentEvent.attachments.length > 0 ? currentEvent.attachments[0].fileUrl : '';
   
-        //future event information
+        // Future event information
         var futureEventName = futureEvent.summary;
         var futureEventStartDate = new Date(futureEvent.start.dateTime);
         var futureEventImage = futureEvent.attachments && futureEvent.attachments.length > 0 ? futureEvent.attachments[0].fileUrl : '';
   
-        //days until future event
+        // Days until future event
         var currentDate = new Date();
         var daysUntilFutureEvent = Math.ceil((futureEventStartDate - currentDate) / (1000 * 60 * 60 * 24));
   
-        //Update with current event information
+        // Update with current event information
         document.getElementById('current-event-name').textContent = currentEventName;
         document.getElementById('current-event-date').textContent = 'End Date: ' + currentEventEndDate.toLocaleString();
         document.getElementById('current-event-image').src = currentEventImage;
   
-        //Update with future event information
+        // Update with future event information
         document.getElementById('future-event-name').textContent = futureEventName;
         document.getElementById('future-event-date').textContent = 'End Date: (Starts in ' + daysUntilFutureEvent + ' days)';
         document.getElementById('future-event-image').src = futureEventImage;
+  
+        // Capture and update the constantly updated picture
+        html2canvas(document.getElementById('widget'))
+          .then(function(canvas) {
+            var imageDataURL = canvas.toDataURL(); // Convert canvas to a data URL
+            document.getElementById('constantly-updated-picture').src = imageDataURL;
+          })
+          .catch(function(error) {
+            console.error('Error capturing screenshot:', error);
+          });
       })
       .catch(error => {
-        //error
+        // Error
         console.error('Error fetching events from Google Calendar:', error);
       });
-  });
-  
+  });  
