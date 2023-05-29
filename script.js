9c5a3fd27bb6decf65bd2e11361a83dc1a290795
ch1kim0n1 <chikimoni61@gmail.com>
2023-05-29 18:20:34 -0500

    new script, need testing

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
-window.addEventListener('DOMContentLoaded', function() {
-    // Your Google Calendar API credentials
-    var apiKey = 'YOUR_API_KEY';
-    var calendarId = 'YOUR_CALENDAR_ID';
-  
-    // Fetch events from Google Calendar API
-    fetch('https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events?key=' + apiKey)
-      .then(response => response.json())
-      .then(data => {
-        // Get the current event and the future event
-        var currentEvent = data.items[0];
-        var futureEvent = data.items[1];
-  
-        // Current event information
-        var currentEventName = currentEvent.summary;
-        var currentEventEndDate = new Date(currentEvent.end.dateTime);
-        var currentEventImage = currentEvent.attachments && currentEvent.attachments.length > 0 ? currentEvent.attachments[0].fileUrl : '';
-  
-        // Future event information
-        var futureEventName = futureEvent.summary;
-        var futureEventStartDate = new Date(futureEvent.start.dateTime);
-        var futureEventImage = futureEvent.attachments && futureEvent.attachments.length > 0 ? futureEvent.attachments[0].fileUrl : '';
-  
-        // Days until future event
-        var currentDate = new Date();
-        var daysUntilFutureEvent = Math.ceil((futureEventStartDate - currentDate) / (1000 * 60 * 60 * 24));
-  
-        // Update with current event information
-        document.getElementById('current-event-name').textContent = currentEventName;
-        document.getElementById('current-event-date').textContent = 'End Date: ' + currentEventEndDate.toLocaleString();
-        document.getElementById('current-event-image').src = currentEventImage;
-  
-        // Update with future event information
-        document.getElementById('future-event-name').textContent = futureEventName;
-        document.getElementById('future-event-date').textContent = 'End Date: (Starts in ' + daysUntilFutureEvent + ' days)';
-        document.getElementById('future-event-image').src = futureEventImage;
-  
-        // Capture and update the constantly updated picture
-        html2canvas(document.getElementById('widget'))
-          .then(function(canvas) {
-            var imageDataURL = canvas.toDataURL(); // Convert canvas to a data URL
-            document.getElementById('constantly-updated-picture').src = imageDataURL;
-          })
-          .catch(function(error) {
-            console.error('Error capturing screenshot:', error);
-          });
-      })
-      .catch(error => {
-        // Error
-        console.error('Error fetching events from Google Calendar:', error);
+// Load the Google API client library
+function loadGoogleApiClient(callback) {
+  const script = document.createElement('script');
+  script.src = 'https://apis.google.com/js/api.js';
+  script.onload = callback;
+  document.body.appendChild(script);
+}
+
+// Function to handle Google Calendar API initialization
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+// Initialize the Google Calendar API client
+function initClient() {
+  gapi.client.init({
+    apiKey: 'YOUR_API_KEY',
+    clientId: 'YOUR_CLIENT_ID',
+    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+    scope: 'https://www.googleapis.com/auth/calendar.readonly'
+  }).then(function() {
+    // Call the function to fetch calendar data and update the widget
+    fetchCalendarData();
+  });
+}
+
+// Function to fetch data from Google Calendar API
+function fetchCalendarData() {
+  gapi.client.calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    showDeleted: false,
+    singleEvents: true,
+    maxResults: 2,
+    orderBy: 'startTime'
+  }).then(function(response) {
+    const events = response.result.items;
+
+    if (events.length > 0) {
+      // Assuming the first event is "current" and the second event is "future"
+      const currentEvent = events[0];
+      const futureEvent = events[1];
+
+      // Update the "current" event information
+      document.getElementById("current-event-name").textContent = currentEvent.summary;
+      document.getElementById("current-event-date").textContent = currentEvent.end.dateTime;
+      document.getElementById("current-event-image").src = currentEvent.attachments ? currentEvent.attachments[0].fileUrl : '';
+
+      // Update the "future" event information
+      document.getElementById("future-event-name").textContent = futureEvent.summary;
+      document.getElementById("future-event-date").textContent = futureEvent.end.dateTime;
+      document.getElementById("future-event-image").src = futureEvent.attachments ? futureEvent.attachments[0].fileUrl : '';
+
+      // Generate screenshot of the widget
+      html2canvas(document.getElementById("widget")).then(function(canvas) {
+        // Convert the canvas to a data URL
+        const dataUrl = canvas.toDataURL();
+
+        // Create a link element to save the screenshot
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'createdWidget.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
       });
-  });  
+    }
+  }, function(error) {
+    console.error('Error fetching calendar events:', error);
+  });
+}
+
+// Load the Google API client library and initialize the Calendar API
+loadGoogleApiClient(handleClientLoad);
+
+// Schedule periodic widget updates (every hour)
+setInterval(fetchCalendarData, 3600000);
