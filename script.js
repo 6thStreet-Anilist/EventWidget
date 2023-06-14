ff7805c78cc24ec9463649400ce49787f681e1ad
ch1kim0n1 <chikimoni61@gmail.com>
2023-06-14 15:17:00 -0500

    api

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
 // Load the Google Calendar API
-gapi.load('client', initialize);
+gapi.load('client', initClient);
 
-// Initialize the API client library and set up the credentials
-function initialize() {
+function initClient() {
+  // Initialize the API client
   gapi.client.init({
     apiKey: '746541266615-s9b4aejdegh0mufernqk8a6n9cf4de7g.apps.googleusercontent.com',
     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
   }).then(function() {
-    // Call the function to load events from Google Calendar
-    loadEvents();
+    // Call the function to load the calendar events
+    loadCalendarEvents();
   }).catch(function(error) {
-    console.log('Error initializing Google Calendar API:', error);
+    console.log('Error initializing the Google Calendar API: ' + error);
   });
 }
 
-//---------------DATA IS NOT IMPORTING INTO HTML YET------------------
-// Update the widget with the event information
-document.getElementById('eventDate').textContent = "Access +";
-document.getElementById('eventTitle').textContent = "Access +";
-
-// Load events from Google Calendar
-function loadEvents() {
+function loadCalendarEvents() {
+  // Make a request to the Google Calendar API
   gapi.client.calendar.events.list({
     calendarId: '9c007573747b29bbb3089d9b0f8cde06717d3c2abb17db7af599955410dbd368@group.calendar.google.com',
-    timeMin: new Date().toISOString(),
-    showDeleted: false,
+    timeMin: (new Date()).toISOString(),
+    maxResults: 1,
+    orderBy: 'startTime',
     singleEvents: true,
-    orderBy: 'startTime'
   }).then(function(response) {
     var events = response.result.items;
-    
     if (events.length > 0) {
-      var event = events[0]; // Assuming the first event is the next one
-      var eventDate = new Date(event.start.dateTime);
+      var event = events[0];
       var eventTitle = event.summary;
+      var eventDate = event.start.dateTime || event.start.date;
       
-      // Update the widget with the event information
-      document.getElementById('eventDate').textContent = eventDate.toLocaleDateString();
+      // Set the event title and date in the widget
       document.getElementById('eventTitle').textContent = eventTitle;
-    } else {
-      console.log('No upcoming events found.');
+      document.getElementById('eventDate').textContent = eventDate;
     }
   }).catch(function(error) {
-    console.log('Error loading events:', error);
+    console.log('Error loading calendar events: ' + error);
   });
 }
